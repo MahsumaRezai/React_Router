823ff0640702eadaf721e1e922170b8dac5162b7
Masoumeh Rezai <you@example.com>
2023-07-03 11:17:00 +0430

    add style for Porduct

--- src/pages/Proudect.js
+++ src/pages/Proudect.js
@@ -1,9 +1,10 @@
 import Cartform from "../componet/AddUser/Cartfrom";
+import classes from './Prodouct.module.css'
 
 const Products = () => {
     return (
         <Cartform>
-            <h2>Welcome to Products page</h2>
+            <h2 className={classes.title}>Welcome to Products page</h2>
         </Cartform>
 
 
